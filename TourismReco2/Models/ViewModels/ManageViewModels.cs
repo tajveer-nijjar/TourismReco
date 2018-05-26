0928cde77c57e6f4b7b61dcd1d9b22acbb33ea0d
tajveer singh nijjar <taj_nijjar2002@yahoo.co.in>
2018-05-26 18:59:28 +1200

    - Moved view models to separate folder

--- TourismReco2/Models/ViewModels/ManageViewModels.cs
+++ TourismReco2/Models/ViewModels/ManageViewModels.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
 
-namespace TourismReco2.Models
+namespace TourismReco2.Models.ViewModels
 {
     public class IndexViewModel
     {
