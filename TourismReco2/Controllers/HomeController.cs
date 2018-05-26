72143ea485acd697434df4573963fdf5cd21ea15
tajveer singh nijjar <taj_nijjar2002@yahoo.co.in>
2018-05-26 12:40:01 +1200

    - No logic yet.

--- TourismReco2/Controllers/HomeController.cs
+++ TourismReco2/Controllers/HomeController.cs
@@ -26,5 +26,17 @@ namespace TourismReco2.Controllers
 
             return View();
         }
+
+        public ActionResult RecommendationsForm()
+        {
+
+            return View();
+        }
+
+        public ActionResult Recommendations()
+        {
+
+            return View();
+        }
     }
 }
---
+++ TourismReco2/Scripts/custom.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+
+    $("input[name=includeRoute]").change(function () {
+        var x = $("#weatherInformation");
+        //if ($("#planRouteYes").prop("checked", true)) {
+        if (this.value === "yes") {
+            x.removeClass('hide_it');
+        } else {
+            $("#weatherInformation").addClass('hide_it');
+        }
+
+    });
+});
+
+$(function () {
+    $('#colour-variations ul').styleSwitcher({
+        defaultThemeId: 'theme-switch',
+        hasPreview: false,
+        cookie: {
+            expires: 30,
+            isManagingLoad: true
+        }
+    });
+    $('.option-toggle').click(function () {
+        $('#colour-variations').toggleClass('sleep');
+    });
+});
