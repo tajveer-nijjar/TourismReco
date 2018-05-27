fbce4dc73a43ae45a44189506a316c6d0bb263ed
tajveer singh nijjar <taj_nijjar2002@yahoo.co.in>
2018-05-27 17:45:13 +1200

    - Formatting of ClanPriority page done.

--- TourismReco2/App_Start/BundleConfig.cs
+++ TourismReco2/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace TourismReco2
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
