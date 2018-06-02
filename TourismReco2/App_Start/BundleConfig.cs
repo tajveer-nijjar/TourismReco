0d8745ed72606d6bd3fa52261e5fcf96a9bd8740
tajveer singh nijjar <taj_nijjar2002@yahoo.co.in>
2018-06-02 16:42:23 +1200

    - Layout of homepage set.

--- TourismReco2/App_Start/BundleConfig.cs
+++ TourismReco2/App_Start/BundleConfig.cs
@@ -26,6 +26,7 @@ namespace TourismReco2
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/RecoForm.css",
+                      "~/Content/HomePage.css",
                       
                       "~/Content/Site.css"));
         }
